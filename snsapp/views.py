9fc64c70acb3699daa2173438dded49572415c02
github mascotas03 <sotauwami@gmail.com>
2023-07-15 17:25:00 +0900

    ユーザーの重複を防ぐ機能の追加

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-# Create your views here.
+# Create your views here. function based view
 
 def signupfunc(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = User.objects.create_user(username, "", password)
-        print("this is post method")
-        print(request.POST)
-    else:
-        print("this is not post method")
+        username2 = request.POST["username"]
+        password2 = request.POST["password"]
+        try:
+            User.objects.get(username=username2)
+            return render(request, "signup.html", {"error":"このユーザーは登録されています"})
+        except:
+            user = User.objects.create_user(username2, "", password2)
+            return render(request, "signup.html",{"some":"shino"})  #templateとdataを組み合わせたものをレスポンスする
     return render(request, "signup.html",{"some":"shino"})  #templateとdataを組み合わせたものをレスポンスする
