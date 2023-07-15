9f738eff1c00d44f95df4d788c4cf7cc8d704048
github mascotas03 <sotauwami@gmail.com>
2023-07-15 17:52:18 +0900

    login機能の実装

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here. function based view
 
@@ -12,5 +14,17 @@ def signupfunc(request):
             return render(request, "signup.html", {"error":"このユーザーは登録されています"})
         except:
             user = User.objects.create_user(username2, "", password2)
-            return render(request, "signup.html",{"some":"shino"})  #templateとdataを組み合わせたものをレスポンスする
+            return render(request, "signup.html",{"some":"shino"})  
     return render(request, "signup.html",{"some":"shino"})  #templateとdataを組み合わせたものをレスポンスする
+
+def loginfunc(request):
+    if request.method == "POST":
+        username2 = request.POST["username"]
+        password2 = request.POST["password"]
+        user = authenticate(request, username=username2, password=password2)   #userの認証
+        if user is not None:
+            login(request, user)
+            return redirect("signup")
+        else:
+            return redirect('login')
+    return render(request, "login.html")
