7c57ce47e2fae1a0a13cb10e001cb0d117bbc076
github mascotas03 <sotauwami@gmail.com>
2023-07-16 13:24:48 +0900

    modelsの設定と画像のURL設定

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import SnsModel
 # Register your models here.
+
+#管理画面で読み込ませてあげる必要あり
+admin.site.register(SnsModel)
--- snsapp/models.py
+++ snsapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class SnsModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    #写真を細かくフォルダ分けしたい場合、uplpad_toに指定する。
+    #今回は一つのプロジェクトだからsettingsの方でBASE指定したファイルに入れるため、ブランクでいい。
+    images = models.ImageField(upload_to="")
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readwho = models.CharField(max_length=200)
+    
+      
+    
